553ddfb4b69d1259c93e2236944f8e99da3322ba
Masaya Suzuki <suzukimasaya428@gmail.com>
2024-12-15 13:28:42 +0900

    fix

--- scripts/pr_format/pr_format/format.sh
+++ scripts/pr_format/pr_format/format.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-uv sync --dev
 uv tool run autopep8 --exit-code --in-place --recursive .
 uv tool run pyink --config .python-black .
 uv tool run isort --sp .isort.cfg .
--- scripts/pr_test_hato_bot/pr_test/test.sh
+++ scripts/pr_test_hato_bot/pr_test/test.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 cp .env.example .env
-source .venv/bin/activate
 uv run python -m unittest
