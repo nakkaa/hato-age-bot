dbf1f2ef3bbaea4a405f99277167744f3cefad66
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-01-08 22:52:13 +0900

    Update format.sh

--- scripts/pr_format/pr_format/format.sh
+++ scripts/pr_format/pr_format/format.sh
@@ -6,10 +6,8 @@ tag_version="$(yq '.jobs.pr-super-lint.steps[-1].uses | line_comment' .github/wo
 pyink_version="$(docker run --rm --entrypoint '' "ghcr.io/${tag_name}-${tag_version}" /bin/sh -c 'pyink --version' | grep pyink | awk '{ print $2 }')"
 sed -i -e "s/pyink==.*\"/pyink==${pyink_version}\"/g" pyproject.toml
 uv sync --dev
-sudden_death_url="$(yq .tool.uv.sources.sudden-death.git pyproject.toml)"
 
-if [ "$sudden_death_url" != 'null' ]; then
-	sudden_death_branch="$(yq .tool.uv.sources.sudden-death.branch pyproject.toml)"
+if [ "$(yq .tool.uv.sources.sudden-death.git pyproject.toml)" != 'null' ]; then
 	uv lock --upgrade-package sudden-death
 fi
 
